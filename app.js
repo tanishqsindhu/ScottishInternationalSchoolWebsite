cb6f3210b12545100142fe246b5370c84fae555f
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-05-16 15:18:57 +0530

    changed eventpage

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.post('/news-events/add',(req,res)=>{
 })
 app.get('/news-events/:id',(req,res)=>{
     // const errorMessage='underConstruction'
-    res.render('newsAndEvents/eventpage');
+    res.render('newsAndEvents/EventPage');
 })
 app.get('/news-events/:id/edit',(req,res)=>{
     res.render('newsAndEvents/editNewsAndEvents');
