0abc316f5c1ab2aa9ed3574a59f8f78e1d1a16c5
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-05-27 21:57:52 +0530

    changed news and events to routes and controllers

--- controllers/newsAndEvents.js
+++ controllers/newsAndEvents.js
@@ -1,2 +1,44 @@
-const NewsArticles = require('./models/newsAndEvents');
+const NewsArticles = require('../models/newsAndEvents');
 const{cloudinary} = require('../cloudinary');
+
+module.exports.home=async(req,res)=>{
+    const news = await NewsArticles.find({});
+    res.render('newsAndEvents/homePage',{news});
+}
+
+module.exports.renderAddForm = (req,res)=>{
+    res.render('newsAndEvents/addNewsAndEvents');
+}
+
+module.exports.add=async(req,res)=>{
+    const imgs=req.files.map(f=>({url:f.path,filename:f.filename}));
+    const newsArticle = new NewsArticles({...req.body.article})
+    newsArticle.images.push(...imgs);
+    await newsArticle.save();
+    const news = await NewsArticles.find({});
+    res.render('newsAndEvents/homePage',{news});
+}
+
+module.exports.newsArticle = async(req,res)=>{
+    const news = await NewsArticles.findById(req.params.id)
+    res.render('newsAndEvents/EventPage',{news});
+}
+
+module.exports.articleEdit=async(req,res)=>{
+    const news = await NewsArticles.findById(req.params.id);
+    res.render('newsAndEvents/editNewsAndEvents',{news});
+}
+
+module.exports.articleUpdate=async(req,res)=>{
+    const imgs=req.files.map(f=>({url:f.path,filename:f.filename}));
+    const newsArticle = await NewsArticles.findByIdAndUpdate(req.params.id, { ...req.body.article });
+    newsArticle.images.push(...imgs);
+    if(req.body.deleteImages){
+        for(let filename of req.body.deleteImages){
+            await cloudinary.uploader.destroy(filename);
+        }
+        await newsArticle.updateOne({$pull:{images:{filename:{$in:req.body.deleteImages}}}})
+    }
+    await newsArticle.save();
+    res.redirect(`/news-events/${newsArticle._id}`);
+}
--- routes/newsAndEvents.js
+++ routes/newsAndEvents.js
@@ -3,46 +3,23 @@ const router = express.Router();
 const app = express();
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn,isOffice, validateStudent } = require('../middleware');
-const newsLetter=require('../controllers/newsLetter');
+const newsAndEvents=require('../controllers/newsAndEvents');
 const multer=require('multer');
 const {storage} = require('../cloudinary')
 const upload=multer({storage});
 
-app.get('/',catchAsync(async(req,res)=>{
-    const news = await NewsArticles.find({});
-    res.render('newsAndEvents/homePage',{news});
-}))
-app.get('/news-events/add',(req,res)=>{
-    res.render('newsAndEvents/addNewsAndEvents');
-})
-app.post('/news-events/add',upload.array('image'),catchAsync(async(req,res)=>{
-    const imgs=req.files.map(f=>({url:f.path,filename:f.filename}));
-    const newsArticle = new NewsArticles({...req.body.article})
-    newsArticle.images.push(...imgs);
-    await newsArticle.save();
-    const news = await NewsArticles.find({});
-    res.render('newsAndEvents/homePage',{news});
-}))
-app.get('/news-events/:id',catchAsync(async(req,res)=>{
-    const news = await NewsArticles.findById(req.params.id)
-    res.render('newsAndEvents/EventPage',{news});
-}))
-app.get('/news-events/:id/edit',catchAsync(async(req,res)=>{
-    const news = await NewsArticles.findById(req.params.id)
-    res.render('newsAndEvents/editNewsAndEvents',{news});
-}))
-app.put('/news-events/:id',upload.array('image'),catchAsync(async(req,res)=>{
-    const imgs=req.files.map(f=>({url:f.path,filename:f.filename}));
-    const newsArticle = await NewsArticles.findByIdAndUpdate(req.params.id, { ...req.body.article });
-    newsArticle.images.push(...imgs);
-    if(req.body.deleteImages){
-        for(let filename of req.body.deleteImages){
-            await cloudinary.uploader.destroy(filename);
-        }
-        await newsArticle.updateOne({$pull:{images:{filename:{$in:req.body.deleteImages}}}})
-    }
-    await newsArticle.save();
-    res.redirect(`/news-events/${newsArticle._id}`);
-}))
+router.route('/')
+.get(catchAsync(newsAndEvents.home));
+
+router.route('/add')
+.get(isLoggedIn,newsAndEvents.renderAddForm)
+.post(upload.array('image'),catchAsync(newsAndEvents.add));
+
+router.route('/:id')
+.get(catchAsync(newsAndEvents.newsArticle))
+.put(isLoggedIn,upload.array('image'),catchAsync(newsAndEvents.articleUpdate));
+
+router.route('/:id/edit')
+.get(isLoggedIn,catchAsync(newsAndEvents.articleEdit));
 
 module.exports = router;
